40f42203036a4294ce1a850ba76a4b23ae7b6dbc
Админ <qwertnemo@gmail.com>
2014-10-20 17:35:02 +0400

    Create Les10

---
+++ Les10/src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+
+public class Solution{
+	
+	public static void main(String[] args){
+		
+		
+		Random a = new Random(); 
+	
+		ArrayList<Integer> list = new ArrayList<Integer>();
+	
+		for (int i = 0; i < 5; i++) {
+		
+			list.add(a.nextInt(10));		
+		
+		}
+	
+	
+		for (Integer w : list) {
+			System.out.println(w);		
+		}
+	
+		System.out.println("Всего элементов массива " + list.size()); 
+	
+		list.set(2, 9);
+	
+		System.out.println(list.get(1));
+	
+		for (Integer w : list) {
+			System.out.println(w);		
+		}
+	
+	
+	}
+
+}	

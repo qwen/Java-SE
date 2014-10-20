5547a848fc6c7cc57e2ca4e7f6c20c649fe4e33c
DenZen <DenZenqwernemo@mail.com>
2014-10-20 17:42:37 +0400

    Add kommentary

--- Les10/src/Solution.java
+++ Les10/src/Solution.java
@@ -11,7 +11,7 @@ public class Solution{
 		
 		Random a = new Random(); 
 	
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>(); //Коллекция
 	
 		for (int i = 0; i < 5; i++) {
 		

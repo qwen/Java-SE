8a6067a08b6a3f31f13b612f1f796da51deaa80f
Админ <@HP>
2014-10-22 20:36:28 +0400

    Modification exception

--- Les10/src/Solution.java
+++ Les10/src/Solution.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -5,25 +7,44 @@ import java.util.Random;
 
 public class Solution{
 	
-	public static void main(String[] args){
+	
+	
+	public static void main(String[] args) throws Exception {
 		
-		
+		int q = 0;
 		Random a = new Random(); 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Введите количество элементов массива");
+		
+		
+		try {
+			q = Integer.parseInt(reader.readLine());
+		} catch (java.lang.NumberFormatException e) {
+			System.out.println("Вы ввели не тот символ, введите число ");
+			q = Integer.parseInt(reader.readLine());
+		} catch (java.util.NoSuchElementException e) {
+			q = Integer.parseInt(reader.readLine());
+		}
+			
+			
+		
+		
 	
 		ArrayList<Integer> list = new ArrayList<Integer>(); //Коллекция
 	
-		for (int i = 0; i < 5; i++) {		
-			list.add(a.nextInt(10));	//Заполняем случйными числами (0-10)			
+		for (int i = 0; i < q; i++) {		
+			list.add(a.nextInt(10));	//Заполняем случйными числами (0-10)		
 		}
 		
 		for (Integer w : list) {
 			System.out.println(w);		
 		}
 	
-		System.out.println("Всего элементов массива " + list.size()); 
+		/*System.out.println("Всего элементов массива " + list.size()); 
 		System.out.println("Максимальный элемент массива " + Collections.max(list));
-		System.out.println("Минимальный элемент массива " + Collections.min(list));
-
+		System.out.println("Минимальный элемент массива " + Collections.min(list));*/
+		info(list);
+		
 		list.set(2, 99);		//Заменяем второый элемент массива на 99
 	
 		System.out.println("Нулевой элемент массива " + list.get(0));
@@ -34,4 +55,11 @@ public class Solution{
 	
 	
 	}
+	
+	public static void info(ArrayList<Integer> list) {
+		System.out.println("Всего элементов массива " + list.size());
+		System.out.println("Максимальный элемент массива " + Collections.max(list));
+		System.out.println("Минимальный элемент массива " + Collections.min(list));
+	}
+	
 }	

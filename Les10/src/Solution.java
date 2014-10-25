34eb4ee470d5212dae98c251060dc2fdb6526b99
DenZen <DenZenqwernemo@mail.com>
2014-10-25 10:47:00 +0400

    Modification exeption 2

--- Les10/src/Solution.java
+++ Les10/src/Solution.java
@@ -4,19 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-
 public class Solution{
-	
-	
-	
+		
 	public static void main(String[] args) throws Exception {
 		
-		int q = 0;
+		int q;
+		int s;
 		Random a = new Random(); 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
 		System.out.println("Введите количество элементов массива");
-		
-		
+				
 		try {
 			q = Integer.parseInt(reader.readLine());
 		} catch (java.lang.NumberFormatException e) {
@@ -25,25 +23,22 @@ public class Solution{
 		} catch (java.util.NoSuchElementException e) {
 			q = Integer.parseInt(reader.readLine());
 		}
-			
-			
 		
+		System.out.println("Введите максимальное число рандомного заполнения массива");
+		s = Integer.parseInt(reader.readLine());
 		
 	
 		ArrayList<Integer> list = new ArrayList<Integer>(); //Коллекция
 	
 		for (int i = 0; i < q; i++) {		
-			list.add(a.nextInt(10));	//Заполняем случйными числами (0-10)		
+			list.add(a.nextInt(s));	//Заполняем случйными числами (0-s)		
 		}
 		
-		for (Integer w : list) {
-			System.out.println(w);		
+		for (Integer i : list) {
+			System.out.println(i);		
 		}
 	
-		/*System.out.println("Всего элементов массива " + list.size()); 
-		System.out.println("Максимальный элемент массива " + Collections.max(list));
-		System.out.println("Минимальный элемент массива " + Collections.min(list));*/
-		info(list);
+		info(list);		//Обращаемся к методу info с параметром list
 		
 		list.set(2, 99);		//Заменяем второый элемент массива на 99
 	
@@ -51,8 +46,7 @@ public class Solution{
 	
 		for (Integer w : list) {
 			System.out.println(w);		
-		}
-	
+		}	
 	
 	}
 	
